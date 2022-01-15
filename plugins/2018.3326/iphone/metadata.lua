125b1c7df72227a92e3a69875b0b85c97843bec9
Scott Harrison <scottrules44@gmail.com>
2022-01-15 11:30:25 -0500

    not currently by supported by Appodeal replacing with stub

--- plugins/2018.3326/iphone/metadata.lua
+++ plugins/2018.3326/iphone/metadata.lua
@@ -3,8 +3,8 @@ local metadata =
 	plugin =
 	{
 		format = 'staticLibrary',
-		staticLibs = { 'APDChartboostAdapter', },
-		frameworks = { 'Chartboost', },
+		staticLibs = { },
+		frameworks = { },
 		frameworksOptional = {},
 	},
 }
